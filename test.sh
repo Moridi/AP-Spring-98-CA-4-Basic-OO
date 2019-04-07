76f74a5c8436e72aeab417f18c3f108955ca84ed
moridi <m.moridi.2009@gmail.com>
2019-04-08 00:15:07 +0430

    Add test script

---
+++ test.sh
@@ -0,0 +1,16 @@
+unzip A4-"$1".zip
+(cd A4-"$1"/; make)
+cp A4-"$1"/Scheduler.out .
+
+counter=0
+for i in {1..10}
+do
+    if ! ./Scheduler.out < Testcases/input/$i.in | diff -rq Testcases/output/$i.out -; then
+        echo "$i - Failed!"
+    else
+        echo "$i - Passed!"
+        counter=$(( counter + 1 ));
+    fi
+done
+
+echo -e "\n########\t$counter test cases passed!\t########"
